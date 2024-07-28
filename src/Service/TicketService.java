bfb54a98c3fa5a468373cbecf01ce9044eb42eea
JustAnAverageMax <theaveragemaximum@gmail.com>
2024-07-28 14:28:38 +0300

    Add Model and Service packages

---
+++ src/Service/TicketService.java
@@ -0,0 +1,14 @@
+package Service;
+
+import Model.Ticket;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class TicketService {
+    public static void main(String[] args) {
+        List<Ticket> ticketStorage = new ArrayList<>();
+
+
+    }
+}
