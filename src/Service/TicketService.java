34b686682e1be36726dbd646bb9d54d7f650995a
JustAnAverageMax <theaveragemaximum@gmail.com>
2024-07-28 16:07:20 +0300

    Add ticket storage

--- src/Service/TicketService.java
+++ src/Service/TicketService.java
@@ -1,14 +1,47 @@
 package Service;
 
 import Model.Ticket;
+import constants.ConcertHall;
+import constants.StadiumSector;
 
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 
 public class TicketService {
     public static void main(String[] args) {
-        List<Ticket> ticketStorage = new ArrayList<>();
+        StadiumSector[] sectors = StadiumSector.values();
+        ConcertHall[] halls = ConcertHall.values();
 
+        Random random = new Random();
+        List<Ticket> storage = new ArrayList<>();
 
+        for (int i = 0; i < 10; i++) {
+            BigDecimal randomPrice = BigDecimal
+                    .valueOf(10 + (100 - 10) * random.nextDouble());
+            BigDecimal randomMaxBackpackWeight = BigDecimal
+                    .valueOf(5 + (20 - 5) * random.nextDouble());
+            StadiumSector randomStadiumSector = sectors[random.nextInt(sectors.length)];
+            ConcertHall randomConcertHall = halls[random.nextInt(halls.length)];
+            boolean isPromo = random.nextBoolean();
+            String randomEventCode = String.valueOf(random.nextInt(900) + 100);
+            Ticket currentTicket = new Ticket()
+                    .withPrice(randomPrice)
+                    .withStadiumSector(randomStadiumSector)
+                    .withPromo(isPromo)
+                    .withMaxBackpackWeight(randomMaxBackpackWeight)
+                    .withEventCode(randomEventCode)
+                    .withConcertHall(randomConcertHall)
+                    .withTime(LocalDateTime.now()
+                            .plusMonths(random.nextInt(5))
+                            .plusDays(random.nextInt(30))
+                            .plusHours(random.nextInt(24))
+                            .withMinute(random.nextInt(12) * 5)
+                    );
+            System.out.println(currentTicket);
+            storage.add(currentTicket);
+        }
     }
 }
